75e7047f98059cfd67c47fa898992292ed32047a
Preesa Wong <ree.wong.career@gmail.com>
2024-11-25 18:51:20 -0500

    Add split_pdf()

--- pdf_tools.py
+++ pdf_tools.py
@@ -1,8 +1,8 @@
 
 import os
-from PyPDF2 import PdfWriter, PdfReader, PdfFileMerger, PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfWriter, PdfReader, PdfFileMerger, PdfFileWriter
 
-def split_pdf():
+def split_pdf2():
 	if not os.path.isfile(file_name):
 		print(f"The file {file_name} does not exist.")
 	else:
@@ -24,17 +24,30 @@ def split_pdf():
 			merge_pdf.write(output_pdf)
 
 def pdf_splitter(path):
-	fname = os.path.splitext(os.path.basename(path))[0]
-	print()
-	pdf = PdfFileReader(path)
+	# os.path.basename(path) returns file_name.pdf
+	# os.path.splitext(os.path.basename(path)) returns ('file_name', '.pdf')
+	f_name = os.path.splitext(os.path.basename(path))[0]
+	pdf = PdfReader(path)
 	for page in range(pdf.getNumPages()):
 		pdf_writer = PdfFileWriter()
-		pdf_writer.addPage(pdf.getPage(page))
+		# pdf_writer.addPage(pdf.getPage(page))
+		pdf_writer.addPage(len(pdf.pages))
 		output_filename = '{}_page_{}.pdf'.format(
-			fname, page+1)
+			f_name, page+1)
 		with open(output_filename, 'wb') as out:
 			pdf_writer.write(out)
 			print('Created: {}'.format(output_filename))
+
+from PyPDF2 import PdfWriter
+
+def split_pdf(): 
+	merger = PdfWriter()
+
+	for pdf in ["file1.pdf", "file2.pdf", "file3.pdf"]:
+		merger.append(pdf)
+
+	merger.write("merged-pdf.pdf")
+	merger.close()
 
 if __name__ == "__main__": 
 	file_name = input("Enter the name of the PDF file to split: ").strip()
