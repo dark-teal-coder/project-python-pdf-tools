061b5cc842479db214aa16ce27f5b39b058be30b
Preesa Wong <ree.wong.career@gmail.com>
2024-11-25 20:12:39 -0500

    Add split_pdf()

--- pdf_tools.py
+++ pdf_tools.py
@@ -1,6 +1,8 @@
 
 import os
 from PyPDF2 import PdfWriter
+from PyPDF2 import PdfReader, PdfWriter
+
 
 def merge_pdfs(pdf_files_list, output_folder_name): 
 	merger = PdfWriter()
@@ -9,7 +11,17 @@ def merge_pdfs(pdf_files_list, output_folder_name):
 	merger.write(f".//{output_folder_name}//Merged PDF.pdf")
 	merger.close()
 
+def split_pdf(input_folder_name, output_folder_name): 
+	with open(f".//{input_folder_name}//Sample PDF File 4.pdf", 'rb') as infile:
+		reader = PdfReader(infile)
+		writer = PdfWriter()
+		# writer.addPage(reader.getPage(0))
+		writer.add_page(reader.pages[0])
+		with open(f".//{output_folder_name}//Split PDF.pdf", 'wb') as outfile:
+			writer.write(outfile)
+
 if __name__ == "__main__": 
+	## Print menu function options
 	menu_message = """What would you like to do with your PDF file(s)? 
 	1.) Merge PDF files
 	2.) Split a PDF file
@@ -22,8 +34,11 @@ if __name__ == "__main__":
 		os.makedirs(input_folder_name)
 	if not os.path.exists(output_folder_name):
 		os.makedirs(output_folder_name)
+	## Function menu
 	if option == 1: 
 		## Get the names of the PDF files to be merged and add the folder name to the front of them  
 		input_files_list = os.listdir(f".//{input_folder_name}")
 		input_files_list_dir_name = [f".//{input_folder_name}//{input_f}" for input_f in input_files_list]
 		merge_pdfs(input_files_list_dir_name, output_folder_name)
+	elif option == 2:
+		split_pdf(input_folder_name, output_folder_name)
