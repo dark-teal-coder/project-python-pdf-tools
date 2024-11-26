3619b05394cc0f742d380314eb810dbd6eb3738e
Preesa Wong <ree.wong.career@gmail.com>
2024-11-25 19:35:34 -0500

    Add the part to create the output folder if it doesn't exist yet

--- pdf_tools.py
+++ pdf_tools.py
@@ -40,17 +40,20 @@ def pdf_splitter(path):
 
 from PyPDF2 import PdfWriter
 
-def merge_pdfs(pdf_files_list): 
+def merge_pdfs(pdf_files_list, output_folder_name): 
 	merger = PdfWriter()
 	for pdf in pdf_files_list:
 		merger.append(pdf)
-	merger.write("merged-pdf.pdf")
+	merger.write(f".//{output_folder_name}//Merged PDF.pdf")
 	merger.close()
 
 if __name__ == "__main__": 
 	# file_name = input("Enter the name of the PDF file to split: ").strip()
 	# pdf_splitter(file_name)
 	input_folder_name = "input"
+	output_folder_name = "output"
 	input_files_list = os.listdir(f".//{input_folder_name}")
 	input_files_list_dir_name = [f".//{input_folder_name}//{input_f}" for input_f in input_files_list]
-	merge_pdfs(input_files_list_dir_name)
+	if not os.path.exists(output_folder_name):
+		os.makedirs(output_folder_name)
+	merge_pdfs(input_files_list_dir_name, output_folder_name)
